14946c55b32f531811b94f582c3a059e388b0d27
wavezboy <sanusifatah16@gmail.com>
2023-12-15 13:07:54 +0100

    okay

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,47 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 interface card {
-  src : string
+  src: string;
+  id: number;
 }
-
 
 const cardImages = [
-  {"src": "/img/helmet-1.png"},
-  {"src": "/img/potion-1.png"},
-  {"src": "/img/ring-1.png"},
-  {"src": "/img/scroll-1.png"},
-  {"src": "/img/shield-1.png"},
-  {"src": "/img/sword-1.png"},
-]
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
 
 function App() {
-  const [cards, setCards] = useState<card[]>([])
-  const [turns, setTurns] = useState<number>()
-  const [cover, setCover] = useState("cover")
-  const [original, setOriginal] = useState("original")
+  const [cards, setCards] = useState<card[]>([]);
+  const [turns, setTurns] = useState<number>();
   // shuffle cards
   const shuffleCards = () => {
-
     const shuffledCards = [...cardImages, ...cardImages]
-     .sort(()=> Math.random() - 0.5)
-     .map((card)=>({...card, id: Math.random()}))
-    setCards(shuffledCards)
-    setTurns(0)
-  
-  }
-
-
-  const makeClick = () => {
-    setCover("original")
-    setOriginal("cover")
-  }
-
-
-  console.log(cards, turns)
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+    setCards(shuffledCards);
+    setTurns(0);
+  };
 
   return (
-    
-   <div className='App'>
-    <h1>
-      Magic Match
-    </h1>
-    <button onClick={shuffleCards}>
-      New Game
-    </button>
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
 
-  <div className='cd'>
-    
-   {cards.map((card, i)=>(
-    <div className='container' onClick={makeClick}  key={i}>
-      <img className={cover} src="/img/cover.png" alt="" />
-      <img className={original} src={card.src} alt="" />
+      <div className="cd">
+        {cards.map((card, i) => (
+          <div className="container" key={i}>
+            <img src="/img/cover.png" alt="" />
+            <img src={card.src} alt="" />
+          </div>
+        ))}
+      </div>
     </div>
-  ))}
-     </div>
-   </div>
-  )
+  );
 }
 
-export default App
+export default App;
