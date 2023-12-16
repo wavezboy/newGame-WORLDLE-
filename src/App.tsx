758844995c498ba608820f7958bae0071ca2a2b8
wavezboy <sanusifatah16@gmail.com>
2023-12-16 12:12:18 +0100

    project done

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,11 @@ const cardImages = [
 
 function App() {
   const [cards, setCards] = useState<card[]>([]);
-  const [turns, setTurns] = useState<number>();
+  const [turns, setTurns] = useState<number>(0);
+  const [firstGuess, setFirstGuess] = useState<card | null>();
+  const [secondGuess, setSecondGuess] = useState<card | null>();
+  const [correctGuess, setCorrectGuess] = useState<string[]>([]);
+
   // shuffle cards
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
@@ -27,19 +31,72 @@ function App() {
     setTurns(0);
   };
 
+  const handleClick = (card: card) => {
+    if (firstGuess) {
+      setSecondGuess(card);
+
+      setTimeout(() => {
+        checkCorrectGuess(card);
+      }, 100);
+    } else {
+      setFirstGuess(card);
+    }
+  };
+
+  const checkCorrectGuess = (card: card) => {
+    if (firstGuess?.src === card?.src) {
+      setCorrectGuess([...correctGuess, card.src]);
+    }
+
+    setFirstGuess(null);
+    setSecondGuess(null);
+    setTurns((prev) => prev + 1);
+  };
+
+  // console.log(firstGuess);
+  // console.log(secondGuess);
+  console.log(correctGuess);
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button onClick={shuffleCards}>New Game</button>
+      <button
+        onClick={() => {
+          setCorrectGuess([]);
+          shuffleCards();
+        }}
+      >
+        New Game
+      </button>
 
       <div className="cd">
         {cards.map((card, i) => (
-          <div className="container" key={i}>
-            <img src="/img/cover.png" alt="" />
-            <img src={card.src} alt="" />
+          <div
+            className="container"
+            key={i}
+            onClick={() => {
+              handleClick(card);
+            }}
+          >
+            <img src="/img/cover.png" alt="" className="cover" />
+            <img
+              src={card.src}
+              alt=""
+              className={
+                firstGuess?.id === card.id ||
+                secondGuess?.id === card.id ||
+                correctGuess.includes(card.src)
+                  ? "cover"
+                  : ""
+              }
+            />
           </div>
         ))}
       </div>
+
+      <p>
+        No of Turns: <span>{turns}</span>
+      </p>
     </div>
   );
 }
